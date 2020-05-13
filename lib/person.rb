110920b795987c692a1ea8de04cf99d431ba1e10
frod25 <frod25>
2020-05-13 13:51:18 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,75 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def index_range_check(new_index)
+        if new_index <= 10 && new_index >= 0
+            new_index
+        elsif new_index < 0
+            0
+        elsif new_index > 10
+            10
+        end
+    end
+    
+    def happiness=(new_index)
+        @happiness = self.index_range_check(new_index)
+    end
+
+    def hygiene=(new_index)
+        @hygiene = self.index_range_check(new_index)
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        say = "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        say = "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        say = "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            say = "blah blah blah blah blah"
+        end
+    end
+end
