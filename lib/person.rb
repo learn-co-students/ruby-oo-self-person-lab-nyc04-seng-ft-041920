7b16b643545a1f965056a1e4dee2cbc3f9aad22c
fpolanco <polanco.franchell@gmail.com>
2020-05-13 18:50:33 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,93 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness
+        
+        if @happiness > 10
+            @happiness = 10
+        elsif
+         @happiness < 0
+            @happiness = 0
+        else 
+            @happiness
+        end
+    end
+
+    def hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif
+         @hygiene < 0
+            @hygiene = 0
+        else 
+            @hygiene
+        end
+    end
+
+    # def hygiene= 
+
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return 'all about the benjamins'
+    end
+
+    def take_bath
+        self.hygiene = @hygiene+4
+        p "♪ Rub-a-dub just relaxing in the tub ♫"
+        
+    end
+
+    def work_out
+        self.hygiene = @hygiene-3
+        self.happiness = @happiness+2
+        p "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(person)
+        self.happiness += 3
+        person.happiness += 3
+        return "Hi #{person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -=2
+            person.happiness -=2
+            p "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness +=1
+            person.happiness +=1
+            p "blah blah sun blah rain"
+        else topic == "other"
+            p "blah blah blah blah blah"
+        end
+    end
+
+end
